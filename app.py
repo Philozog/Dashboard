9d7417da367afb9bf4e15b2ea2109a1091ab548f
philozog <philippe.zoghzoghi@gmail.com>
2026-02-18 22:09:24 -0500

    fixed the requirement list to facilitate the deployment on the plotly cloud.
    fixed the HTML div containers in app.pt to make sure the compiler reads it properly when the code is being run

--- app.py
+++ app.py
@@ -1,28 +1,22 @@
-from dash import Dash, html
+from dash import Dash, html, dcc
 import dash
 import os
 from sqlalchemy import create_engine
 
-base_dir=os.path.dirname(__file__)
-db_path=os.path.join(base_dir,"portfolio.db")
+base_dir = os.path.dirname(__file__)
+db_path = os.path.join(base_dir, "portfolio.db")
 
 engine = create_engine(f"sqlite:///{db_path}")
 
+app = Dash(__name__, use_pages=True)
 
-app = Dash(__name__,use_pages=True)
-
-app.layout = html.Div(
-
-     html.H1("Joe Zoghzoghi Portfolio Dashboard"),
-
+app.layout = html.Div([
+    html.H1("Joe Zoghzoghi Portfolio Dashboard"),
     html.Div([
-        
-    dcc.Link(page["name"], href=page["path"], style={"margin-right": "20px"})
-    for page in dash.page_registry.values()
+        dcc.Link(page["name"], href=page["path"], style={"margin-right": "20px"})
+        for page in dash.page_registry.values()
+    ]),
+    dash.page_container,
 ])
-,
-    [dash.page_container])
-    
-server=app.server
-   
 
+server = app.server
